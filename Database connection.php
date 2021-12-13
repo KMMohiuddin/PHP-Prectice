f2068bc847bbcc82a4a919c0260187e78850f756
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2021-12-14 00:35:58 +0600

    update

--- Database connection.php
+++ Database connection.php
@@ -31,7 +31,7 @@ $username = "root";
 $password = "";
 $dbname = "Class3";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -40,11 +40,7 @@ if ($conn->connect_error) {
 // sql to create table
 $sql = "SELECT * FROM `details`";
 
-if ($conn->query($sql) === TRUE) {
-  echo "Added successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
