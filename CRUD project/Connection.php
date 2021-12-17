b427e7ca6966e67915aa1f518fb10f93c36ce68a
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2021-12-18 01:32:03 +0600

    db connection

--- CRUD project/Connection.php
+++ CRUD project/Connection.php
@@ -1,7 +1,7 @@
-<?php{
+<?php
 $conn = mysqli_connect("localhost","root","","class3");
             // Check connection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-        }
+        ?>
